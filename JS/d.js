4188cd658c97d97e4eae60f9aafd5c8e634d44e3
Dowlet77 <132286379+Dowlet77@users.noreply.github.com>
2024-06-08 01:23:55 +0500

    доделал меню бургер.

---
+++ JS/d.js
@@ -0,0 +1,9 @@
+const iconMenu = document.querySelector('.menu__burger');
+if (iconMenu) {
+    const menuBody = document.querySelector('.burger-menu__body');
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock')
+        menuBody.classList.toggle('_active')
+        iconMenu.classList.toggle('_active')
+    });
+}
