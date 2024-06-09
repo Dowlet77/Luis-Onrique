24c2ae5cad5037fb8878a3489f326c78c0f51813
Dowlet77 <132286379+Dowlet77@users.noreply.github.com>
2024-06-09 19:06:03 +0500

    add swiper.js

--- JS/d.js
+++ JS/d.js
@@ -1,3 +1,11 @@
+new Swiper('.swiper',{
+    pagination:{
+        el:'.swiper-pagination',
+        
+        clickable:true,
+    },
+});
+
 const iconMenu = document.querySelector('.menu__burger');
 if (iconMenu) {
     const menuBody = document.querySelector('.burger-menu__body');
