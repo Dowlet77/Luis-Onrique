e99c68ac8577d394e8838be5d7e3fb89227c7c19
Dowlet77 <132286379+Dowlet77@users.noreply.github.com>
2024-06-12 00:40:06 +0500

    add scroll up

--- JS/d.js
+++ JS/d.js
@@ -1,14 +1,14 @@
-new Swiper('.swiper',{
-    speed:850,
-    parallax:true,
-    pagination:{
-        el:'.swiper-pagination',
-        clickable:true,
-        dynamicBullets:true,
+new Swiper('.swiper', {
+    speed: 850,
+    parallax: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
     },
-    navigation:{
-        nextEl:'.swiper-button-next',
-        prevEl:'.swiper-button-prev'
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
     }
 });
 
@@ -21,3 +21,19 @@ if (iconMenu) {
         iconMenu.classList.toggle('_active')
     });
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTop = document.getElementById("back-to-top");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY >= 600) {
+            backToTop.style.display = "block";
+        } else {
+            backToTop.style.display = "none";
+        }
+    });
+
+    backToTop.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
